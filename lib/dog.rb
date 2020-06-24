5588112ee02260d07c4926a79014f10b4883997b
Ben Hogan <j.benhogan@gmail.com>
2020-06-24 14:24:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Add your code here
+class Dog 
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    save
+  end
+  def all
+    @@all
+  end
+  def print_all
+    puts @@all.name.uniq 
+  end
+  def save
+    @@all << self
+  end
+  def clear_all
+    @@all.clear 
+  end
+end
